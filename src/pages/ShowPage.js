00bb7f5011040f051a3d5d65dca348c93efb5379
yungan9 <geyun105@naver.com>
2023-11-22 01:55:14 +0900

    printDate

--- src/pages/ShowPage.js
+++ src/pages/ShowPage.js
@@ -19,12 +19,20 @@ const ShowPage = () => {
         getPosts(id)
     }, [id])
 
+    const printDate = (timestamp) =>{
+        return new Date(timestamp).toLocaleString();
+    }
+
     if(loading){
         return <LoadingSpinner/>
     }
 
     return <div>
         <h1>{post.title}</h1>
+        <small class="text-muted">
+            Create At: {printDate(post.createdAt)}
+            </small>
+            <hr/>
         <p>{post.body}</p>
     </div>;
     
