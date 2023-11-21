25b894ce949c9e3a589ab2899b26168e36c6b5d8
yungan9 <geyun105@naver.com>
2023-11-21 11:20:35 +0900

    create page 이동

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <Router>
       <NavBar />
-      <div className='container'>
+      <div className='container mt-3'>
         <Switch>
           {routes.map((route) =>{
             return<Route key={route.path} exact path={route.path} component={route.component}/>;
--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import Card from '../components/Card';
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 const ListPage = () => {
     const [posts, setPosts] = useState([]);
@@ -18,7 +19,14 @@ const ListPage = () => {
 
     return (
         <div>
+            <div className="d-flex justify-content-between">
             <h1>Blogs</h1>
+            <div>
+                <Link to ="/blogs/create" className="btn btn-success">
+                Create New
+                </Link>
+            </div>
+            </div>
             {posts.map(post => {
                 return (
                     <Card key={post.id} title={post.title}>
