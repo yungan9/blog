ef20f4973ce407ac2ddc413890f4e059d8a7d589
yungan9 <geyun105@naver.com>
2023-11-28 03:05:09 +0900

    Pagination

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -5,6 +5,7 @@ import { Link} from "react-router-dom/cjs/react-router-dom.min";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import LoadingSpinner from "../components/LoadingSpinner";
 import {bool} from 'prop-types';
+import Pagination from "./Pagination";
 
 
 const BlogList =({isAdmin})=>{
@@ -12,9 +13,24 @@ const BlogList =({isAdmin})=>{
     const history = useHistory();
     const [posts, setPosts] = useState([]);
     const [loading, setLoading] = useState([true]);
+    const [currentPage, setCurrentPage] = useState(1);
 
-    const getPosts = () => {
-        axios.get('http://localhost:3001/posts').then((res) => {
+    const getPosts = (page = 1) => {
+        setCurrentPage(page);
+        let params ={
+            _page:page,
+            _limit: 5,
+            _sort: 'id',
+            _order:'desc',
+        }
+
+        if(!isAdmin){
+            params = {...params, publish:true};
+        }
+
+        axios.get(`http://localhost:3001/posts?`,{
+            params
+        }).then((res) => {
             setPosts(res.data);
             setLoading(false);
         })
@@ -42,23 +58,32 @@ const BlogList =({isAdmin})=>{
         return(<div>No blog posts found</div>)
     }
 
-    return posts.filter(post =>{
-        return isAdmin||post.publish
-    }).map(post => {
-        return (
-            <Card 
-            key={post.id} 
-            title={post.title} 
-            onClick={()=>history.push(`/blogs/${post.id}`)}>
-            {isAdmin ? (<div>
-               < button className="btn btn-danger btn-sm"
-                 onClick={(e) => deleteBlog(e, post.id)}>
-                Delete
-               </button>
-            </div>) :null}
-           </Card>
-        );
-    })
+    const renderBlogList = () =>{
+        return posts.map(post => {
+            return (
+                <Card 
+                key={post.id} 
+                title={post.title} 
+                onClick={()=>history.push(`/blogs/${post.id}`)}>
+                {isAdmin ? (<div>
+                   < button className="btn btn-danger btn-sm"
+                     onClick={(e) => deleteBlog(e, post.id)}>
+                    Delete
+                   </button>
+                </div>) :null}
+               </Card>
+            );
+        })
+    }
+    return (
+        <div>
+            {renderBlogList()}
+            <Pagination 
+            currentPage={currentPage} 
+            numberOfPages={5}
+            onClick={getPosts}/>
+        </div>
+    )
 };
 
 BlogList.propTypes = {
---
+++ src/components/Pagination.js
@@ -0,0 +1,40 @@
+import propTypes from "prop-types";
+
+const Pagination = ({currentPage, numberOfPages, onClick}) =>{
+    return (
+        <nav aria-label="Page navigation example">
+        <ul className="pagination justify-content-center">
+          <li className="page-item disabled">
+            <a className="page-link">Previous</a>
+          </li>
+          {Array(numberOfPages).fill(1).map((value, index) => value + index)
+          .map((pageNumber)=>{
+            return <li
+            key={pageNumber}
+             className={`page-item ${currentPage === pageNumber ? 'active' : ''}`}>
+                <div className="page-link cursor-pointer"
+                onClick={()=>{
+                    onClick(pageNumber);
+                }}
+                >{pageNumber}</div></li>
+
+          })}
+          <li className="page-item">
+            <a className="page-link" href="#">Next</a>
+          </li>
+        </ul>
+      </nav>
+        )
+};
+
+Pagination.propTypes ={
+    currentPage: propTypes.number,
+    numberOfPages: propTypes.number.isRequired,
+    onClick: propTypes.func.isRequired
+}
+
+Pagination.defaultProps ={
+    currentPage: 1
+}
+
+export default Pagination;
